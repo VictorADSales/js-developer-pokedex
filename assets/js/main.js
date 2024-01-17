347ed722a9c1211990e0a71694bc1a9d4612e965
victor delgado <virtor.dsales@gmail.com>
2024-01-16 22:10:34 -0300

    new-javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,47 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const button = document.getElementById('button')
+const modal = document.querySelector('dialog')
+function createDetail() {
+
+}
+_pokemons = []
+
 
 const maxRecords = 151
 const limit = 10
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+    let node = document.createElement('li')
+    node.id = pokemon.number
+    node.className = `pokemon ${pokemon.type}`
+    node.innerHTML = `
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+        
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
+            <img src="${pokemon.photo}"
+                alt="${pokemon.name}">
+        </div>
     `
+    node.addEventListener("click", () => {
+        console.log(pokemon.number);
+    })
+    return node
 }
+
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
+        _pokemons = pokemons
+        const newHtml = pokemons.map(convertPokemonToLi)
+        newHtml.forEach(n => pokemonList.appendChild(n))
+        console.log(newHtml)
     })
 }
 
@@ -45,3 +60,9 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+button.onclick = function() {
+    modal.showModal()
+    
+}
+
